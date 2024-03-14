a40884afcc6123b838622f6528a02538df1bac9e
annie-hola <anhanhanh151199@gmail.com>
2024-03-14 23:01:32 +0700

    Import all the waste images

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ cap = cv2.VideoCapture(2)
 classifier = Classifier('Resources/Model/keras_model.h5', 'Resources/Model/labels.txt')
 imgArrow = cv2.imread('Resources/arrow.png', cv2.IMREAD_UNCHANGED)
 classIDBin = 0
+
+# Import all the waste images
+imgWasteList = []
+pathFolderWaste = "Resources/Waste"
+pathList = os.listdir(pathFolderWaste)
+for path in pathList:
+    imgWasteList.append(cv2.imread(os.path.join(pathFolderWaste, path), cv2.IMREAD_UNCHANGED))
